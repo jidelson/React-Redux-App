d173c068d8df64fb678ac19e4917eb216f867780
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-16 14:32:33 -0700

    got the button working

--- app/src/components/FactsList.js
+++ app/src/components/FactsList.js
@@ -11,41 +11,35 @@ function FactsList(){
     const {isLoading, error, facts} = useSelector(selector);
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchFacts());
-    }, [dispatch])
+    // useEffect(() => {
+    //     dispatch(fetchFacts());
+    // }, [dispatch])
 
     return(
         <div>
             <h1>Joe's Cat Facts</h1>
-
-         
-            {/* <button>Get the facts!</button> */}
+            <button onClick={() => dispatch(fetchFacts())}>Get the facts!</button>
             {isLoading && <h4>Loading cat facts...</h4>}
             {error && <p>Uh oh, you have an error... {error}</p>}
-
             <Card>
             <CardBody body inverse style={{ 
-            backgroundColor: 'lightgrey', 
-            border: '6px dotted red',
-            marginLeft:'15%',
-            marginRight:'15%',
-            marginTop: '2%',
-            marginBottom: '2%',
-            paddingTop: '1%',
-            paddingBottom: '1%',
-            boxShadow: '20px 20px',
-            color: 'brown'
+                backgroundColor: 'lightgrey', 
+                border: '6px dotted red',
+                marginLeft:'15%',
+                marginRight:'15%',
+                marginTop: '2%',
+                marginBottom: '2%',
+                paddingTop: '1%',
+                paddingBottom: '1%',
+                boxShadow: '20px 20px',
+                color: 'brown'
             }}>
             {facts.length > 0 && (
                 <div>
                     {
                         facts.map(fact => (
                             <div key={fact._id}>
-                                
                                 <p>{fact.text}</p>
-                                
-                                
                             </div>
                         ))
                     }
