61ac173f21a5a54080a2c479bc8f4b5e07bc2e8e
jidelson <58210030+jidelson@users.noreply.github.com>
2020-07-16 13:09:53 -0700

    met mvp i believe

--- app/src/components/FactsList.js
+++ app/src/components/FactsList.js
@@ -3,6 +3,8 @@ import {useDispatch,useSelector} from 'react-redux';
 
 import { fetchFacts } from '../actions';
 
+import {Card, CardImg, CardBody} from 'reactstrap';
+
 const selector = state => state;
 
 function FactsList(){
@@ -11,23 +13,46 @@ function FactsList(){
 
     useEffect(() => {
         dispatch(fetchFacts());
-    }, [])
+    }, [dispatch])
 
     return(
         <div>
+            <h1>Joe's Cat Facts</h1>
+
+         
+            {/* <button>Get the facts!</button> */}
             {isLoading && <h4>Loading cat facts...</h4>}
             {error && <p>Uh oh, you have an error... {error}</p>}
+
+            <Card>
+            <CardBody body inverse style={{ 
+            backgroundColor: 'lightgrey', 
+            border: '6px dotted red',
+            marginLeft:'15%',
+            marginRight:'15%',
+            marginTop: '2%',
+            marginBottom: '2%',
+            paddingTop: '1%',
+            paddingBottom: '1%',
+            boxShadow: '20px 20px',
+            color: 'brown'
+            }}>
             {facts.length > 0 && (
                 <div>
                     {
                         facts.map(fact => (
                             <div key={fact._id}>
-                                {fact.text}
+                                
+                                <p>{fact.text}</p>
+                                
+                                
                             </div>
                         ))
                     }
                 </div>
             )}
+            </CardBody>
+            </Card>
         </div>
     )
 }
